7c9c46e7558b4bd771dde76a9fa1fb2633047f94
Aniruddha SG <aniruddha.s.g@gmail.com>
2023-01-30 00:44:09 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-const CONNECTION_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/user-authentication'
+const CONNECTION_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/user-authentication'
 
 mongoose.connect(CONNECTION_URI, { useNewUrlParser: true})
     .then(function(){
